9432c479d5c07f9c2289bdff993e517494f746ad
Vicente Silva <vicente.pspenha@gmail.com>
2019-02-15 22:25:19 -0200

    criação da atividade e classe para pedido | criação do botão na classe main para chamar a atividade pedido

--- app/src/main/java/br/com/student/pedido_simple_adapter_android/atividades/PedidoActivity.java
+++ app/src/main/java/br/com/student/pedido_simple_adapter_android/atividades/PedidoActivity.java
@@ -2,18 +2,12 @@ package br.com.student.pedido_simple_adapter_android.atividades;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-import br.com.student.pedido_simple_adapter_android.R;
-
-public class MainActivity extends AppCompatActivity {
+public class PedidoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void openViewPedido(View view) {
+        setContentView(R.layout.activity_pedido);
     }
 }
