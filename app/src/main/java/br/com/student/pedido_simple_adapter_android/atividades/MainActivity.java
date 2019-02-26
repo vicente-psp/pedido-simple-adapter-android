a819f0c7e5835c12bc012c8799a4de29999d6946
Vicente Silva <vicente.pspenha@gmail.com>
2019-02-25 22:31:24 -0300

    implementado o recebimento de possíveis dados da tela cliente

--- app/src/main/java/br/com/student/pedido_simple_adapter_android/atividades/MainActivity.java
+++ app/src/main/java/br/com/student/pedido_simple_adapter_android/atividades/MainActivity.java
@@ -1,13 +1,17 @@
 package br.com.student.pedido_simple_adapter_android.atividades;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import br.com.student.pedido_simple_adapter_android.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int RETORNO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openViewCliente(View view) {
-        startActivity(new Intent(this, ClienteActivity.class));
+        Intent intent = new Intent(this, ClienteActivity.class);
+        startActivityForResult(intent, RETORNO);
+//        startActivity(intent);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RETORNO) {
+            if (resultCode == Activity.RESULT_OK) {
+                String message = data.getStringExtra("MESSAGE");
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Não deu certo", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
