9c29289dd319a05a57f80b5e936cce91fa01e789
Vicente Silva <vicente.pspenha@gmail.com>
2019-02-25 21:32:18 -0300

    atualização no Main

--- app/src/main/java/br/com/student/pedido_simple_adapter_android/atividades/MainActivity.java
+++ app/src/main/java/br/com/student/pedido_simple_adapter_android/atividades/MainActivity.java
@@ -1,16 +1,13 @@
 package br.com.student.pedido_simple_adapter_android.atividades;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Spinner;
 
 import br.com.student.pedido_simple_adapter_android.R;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
